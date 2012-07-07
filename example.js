601f9c6b81f5b79f6e446285921d55add38748cf
antimatter15 <antimatter15@gmail.com>
2012-07-07 17:38:11 -0400

    got example working and stuff

--- example.js
+++ example.js
@@ -5,11 +5,14 @@ var dht = new DHT.DHT(51414);
 
 // for bootstrapping you need to know a node which already is in the dht
 dht.start();
-dht.bootstrap([ { 'address': 'xxx.xxx.xxx.xxx', 'port': xxx } ]);
+dns.resolve4('router.bittorrent.com', function (err, addresses) {
+	dht.bootstrap([ { 'address': addresses[0], 'port': 6881 } ], function(){
+		var id = DHT.util.hex2buf("640FE84C613C17F663551D218689A64E8AEBEABE");
 
-var id = DHT.util.hex2buf("640FE84C613C17F663551D218689A64E8AEBEABE");
+		dht.lookup(id, function (peers, finished) {
+			console.log("Found more peers: %j", peers);
+			if (finished) console.log("Lookup done");
+		});
+	});
+})
 
-dht.lookup(id, function (peers, finished) {
-	console.log("Found more peers: %j", peers);
-	if (finished) console.log("Lookup done");
-});
